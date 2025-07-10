8857128cc9a742d3f08097ea7365027721c62214
star-discord <star.vesta.legion.kanri@gmail.com>
2025-07-11 08:23:09 +0900

    Update 本文入力をする.js

--- utils/totusuna_setti/modals/本文入力をする.js
+++ utils/totusuna_setti/modals/本文入力をする.js
@@ -1,7 +1,7 @@
 // utils/totusuna_setti/modals/本文入力をする.js
 const fs = require('fs');
 const path = require('path');
-const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, InteractionResponseFlags } = require('discord.js');
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = async function handleContentModal(interaction) {
@@ -19,16 +19,16 @@ module.exports = async function handleContentModal(interaction) {
 
   const json = JSON.parse(fs.readFileSync(dataFile, 'utf-8'));
 
-  if (!json.totsusuna) json.totsusuna = {};
+  if (!json.totusuna) json.totusuna = {};
 
   // UUIDを生成し、本文と設置チャンネルIDなどを記録
   const uuid = uuidv4();
-  json.totsusuna[uuid] = {
+  json.totusuna[uuid] = {
     uuid,
     userId,
     body: inputText,
     installChannelId: interaction.channelId,
-    replicateChannelIds: [], // 複製先チャンネルがある場合に追加
+    replicateChannelIds: [],
   };
 
   // ボタン付きメッセージの送信（Embed）
@@ -48,11 +48,10 @@ module.exports = async function handleContentModal(interaction) {
   const sent = await channel.send({ embeds: [embed], components: [row] });
 
   // メッセージIDを保存
-  json.totsusuna[uuid].messageId = sent.id;
+  json.totusuna[uuid].messageId = sent.id;
 
   // 書き込み保存
   fs.writeFileSync(dataFile, JSON.stringify(json, null, 2), 'utf8');
 
-  // 確認返信
   await interaction.reply({ content: '✅ 本文を保存し、凸スナボタンを設置しました。', flags: InteractionResponseFlags.Ephemeral });
 };
