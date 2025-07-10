89cc693326a8368a81cc598eb1ff29f4704ea7a4
star-discord <star.vesta.legion.kanri@gmail.com>
2025-07-10 19:07:26 +0900

    Update buttons.js

--- utils/totusuna_setti/buttons.js
+++ utils/totusuna_setti/buttons.js
@@ -1,5 +1,26 @@
+const {
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+  ActionRowBuilder,
+} = require('discord.js');
+
 module.exports = {
-  async sendReport(interaction, client) {
-    // 凸スナ報告送信処理をここに記述
+  async open_content_modal(interaction) {
+    const modal = new ModalBuilder()
+      .setCustomId('totusuna_content_modal')
+      .setTitle('本文入力（設置用）');
+
+    const bodyInput = new TextInputBuilder()
+      .setCustomId('main_body')
+      .setLabel('本文（記載ルール含めて記述）')
+      .setStyle(TextInputStyle.Paragraph)
+      .setRequired(true);
+
+    modal.addComponents(
+      new ActionRowBuilder().addComponents(bodyInput)
+    );
+
+    await interaction.showModal(modal);
   }
 };
