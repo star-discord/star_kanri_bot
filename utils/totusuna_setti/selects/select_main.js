6a7693329805b6e405aea074b3b8699355f13b37
star-discord <star.vesta.legion.kanri@gmail.com>
2025-07-11 20:01:05 +0900

    Create select_main.js

---
+++ utils/totusuna_setti/selects/select_main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  customId: 'totsusuna_setti:select_main',
+
+  /**
+   * @param {import('discord.js').StringSelectMenuInteraction} interaction
+   */
+  async handle(interaction) {
+    // 選択値の取得例
+    const selectedChannelId = interaction.values[0];
+
+    // 何か状態を更新する処理など
+
+    // メッセージ更新や状態更新は必要ならここで行う
+
+    // 新しいメッセージを送らずにインタラクションを終える
+    await interaction.deferUpdate();
+  }
+};
