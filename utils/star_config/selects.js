4065383d28f377150821cf358fcaaf510a1dcd83
star-discord <star.vesta.legion.kanri@gmail.com>
2025-07-11 19:59:33 +0900

    Update selects.js

--- utils/star_config/selects.js
+++ utils/star_config/selects.js
@@ -1,6 +1,18 @@
-const path = require('path');
-const { loadHandlers } = require('../../handlerLoader');
+module.exports = {
+  customId: 'totsusuna_setti:select_main',
 
-const findHandler = loadHandlers(path.join(__dirname, 'selects'));
+  /**
+   * @param {import('discord.js').StringSelectMenuInteraction} interaction
+   */
+  async handle(interaction) {
+    // 選択値の取得例
+    const selectedChannelId = interaction.values[0];
 
-module.exports = findHandler;
+    // 何か状態を更新する処理など
+
+    // メッセージ更新や状態更新は必要ならここで行う
+
+    // 新しいメッセージを送らずにインタラクションを終える
+    await interaction.deferUpdate();
+  }
+};
