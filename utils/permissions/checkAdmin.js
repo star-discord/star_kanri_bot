665d3bd9a70ec1beced189cd3b50c6c1629a543a
star-discord <star.vesta.legion.kanri@gmail.com>
2025-07-12 15:34:59 +0900

    Create checkAdmin.js

---
+++ utils/permissions/checkAdmin.js
@@ -0,0 +1,12 @@
+/**
+ * 指定されたメンバーが、adminRoleIds のいずれかを持つか確認
+ * @param {import('discord.js').GuildMember} member
+ * @param {string[]} adminRoleIds
+ * @returns {boolean}
+ */
+function checkAdmin(member, adminRoleIds = []) {
+  if (!member || !member.roles || !Array.isArray(adminRoleIds)) return false;
+  return member.roles.cache.some(role => adminRoleIds.includes(role.id));
+}
+
+module.exports = checkAdmin;
