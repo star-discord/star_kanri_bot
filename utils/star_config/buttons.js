af0294f72104563b8f70cdd62d35b8a968b372ab
star-discord <star.vesta.legion.kanri@gmail.com>
2025-07-13 03:27:53 +0900

    Add files via upload

--- utils/star_config/buttons.js
+++ utils/star_config/buttons.js
@@ -1,6 +1,6 @@
 // utils/star_config/buttons.js
 const path = require('path');
-const { loadHandlers } = require('../../handlerLoader');
+const { loadHandlers } = require('../handlerLoader');
 
 const findHandler = loadHandlers(path.join(__dirname, 'buttons'));
 
