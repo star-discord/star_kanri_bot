4e120f67af71b72fe0c65d29ef51a292cbda602a
star-discord <star.vesta.legion.kanri@gmail.com>
2025-07-13 01:25:23 +0900

    Update buttons.js

--- utils/kpi_setti/buttons.js
+++ utils/kpi_setti/buttons.js
@@ -1,3 +1,5 @@
+// ファイル参照: utils/kpi_setti/buttons.js
+
 const path = require('path');
 const { loadHandlers } = require('../handlerLoader');
 
@@ -5,7 +7,7 @@ const { loadHandlers } = require('../handlerLoader');
 const handlers = loadHandlers(path.join(__dirname, 'buttons'));
 
 /**
- * カスタムIDに一致するボタンハンドラを返す
+ * customId に一致するボタンハンドラを返す
  * @param {string} customId
  * @returns {{ handle(interaction): Promise<void> } | null}
  */
